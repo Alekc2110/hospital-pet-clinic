a0c2d38ee1caae74003fe8de346abaa5c9aca2ff
Oleksandr Korshunovych <korsunonica@gmail.com>
2021-11-10 20:46:30 +0200

    added MapStructPatientMapper

---
+++ src/main/java/com/my/project/petclinic/hospital/persistence/repository/mapper/MapStructPatientMapper.java
@@ -0,0 +1,16 @@
+package com.my.project.petclinic.hospital.persistence.repository.mapper;
+
+import com.my.project.petclinic.hospital.domain.model.Patient;
+import com.my.project.petclinic.hospital.persistence.entity.PatientEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MapStructPatientMapper {
+
+    @Mapping(source = "patient.doctors", target = "doctorList")
+    PatientEntity modelToEntity(Patient patient);
+
+    @Mapping(source = "patientEntity.doctorList", target = "doctors")
+    Patient entityToModel(PatientEntity patientEntity);
+}
